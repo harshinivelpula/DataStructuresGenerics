ae0531b87b63fa1fb188bbf4f94391ca7b477c67
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-07 12:56:05 +0530

    ability to peak&pop from stack till it empty

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace DataStructureGeneric
+﻿using DataStructuresGeneric;
+
+namespace DataStructureGeneric
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("welcome to data structure problem using generics");
+            Console.WriteLine("Hello World!");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+            linkedListStack.Peek(); //top.data
+            linkedListStack.Pop();
+            linkedListStack.IsEmpty();
+            linkedListStack.Display();
+            Console.ReadKey();
         }
     }
 }
